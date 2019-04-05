13b9cc11d74b5307d02335d1cdf2877aafb4021d
Fernando <fernando.fernandez@chghealthcare.com>
2019-04-05 14:21:47 -0600

    faking PATCH and DELETE requests

--- newsite/routes/web.php
+++ newsite/routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', 'PagesController@home');
-// Route::get('/about', 'PagesController@about');
-// Route::get('/contact', 'PagesController@contact');
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
 // Route::get('/projects', 'ProjectsController@index');
 // Route::get('/projects/create', 'ProjectsController@create');
 // Route::get('/projects/{project}', 'ProjectsController@show');
