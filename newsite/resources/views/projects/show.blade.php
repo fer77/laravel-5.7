9d2d73340fc402dcf7d60ef8cbc5f3cbd3b24b7a
Fernando <fernando.fernandez@chghealthcare.com>
2019-04-12 09:17:37 -0600

    created a new view to take a look at each project.

---
+++ newsite/resources/views/projects/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+@section('content')
+<h1>{{ $project->title }}</h1>
+<div class="content">
+    {{ $project->description }}
+</div>
+<p>
+    <a href="/projects/{{ $project->id }}/edit">Edit</a>
+</p>
+@endsection
